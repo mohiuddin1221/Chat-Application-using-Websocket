c2b2b1d6d0e79cb44acf9fc9014d95ebbc5230b7
mohiuddin1221 <topumohiuddin3@gmail.com>
2024-06-29 17:06:39 +0600

    Initial commit on main branch

---
+++ a_rtchat/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
+from .models import ChatGroup
+from .froms import ChatMessageCraeteForm
+
+# Create your views here.
+@login_required
+def chat_view(request):
+    chat_group = get_object_or_404(ChatGroup, group_name='We_Are_Friends')
+    chat_messages = chat_group.chat_message.all()[:30]
+    form = ChatMessageCraeteForm()
+    if request.htmx:
+        form = ChatMessageCraeteForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                'message' : message,
+                'user' : request.user
+            }
+            return render(request, 'a_rtchat/partials/chat_message_p.html', context)
+    context = {
+        'chat_messages': chat_messages,
+        'form':form
+    }
+    return render(request, 'a_rtchat/chat.html', context)
